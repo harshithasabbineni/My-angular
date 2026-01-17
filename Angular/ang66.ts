551056060aad8674a5af1611720e6b94c3397edb
HarshithaSabbinei <harshithasabbineni@gmail.com>
2026-01-17 12:16:12 +0530

    Create ang66.ts

---
+++ Angular/ang66.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { Routes,RouterModule,RouterLink,RouterLinkActive } from '@angular/router';
+@Component({
+  selector: 'app-root',
+  standalone:true,
+  imports: [RouterOutlet,CommonModule,RouterLink,RouterLinkActive],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title='Routing';
+}
