209626253393ba9e21cd83df69ce450f1da9f24b
HarshithaSabbinei <harshithasabbineni@gmail.com>
2026-01-17 10:43:20 +0530

    Add App component with encapsulation set to None

---
+++ Angular/ang63.ts
@@ -0,0 +1,10 @@
+import { Component, ViewEncapsulation} from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+  encapsulation:ViewEncapsulation.None
+})
+export class App {
+  title='Angular20 None Example'
+}
